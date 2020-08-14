421f99a045604d09beb9770be430a6307f197a32
Isaias Quintanilla <quintanillaisaias@gmail.com>
2020-08-14 11:55:41 -0500

    added jumbotron and input for searches

--- src/components/EmployeeSearch.js
+++ src/components/EmployeeSearch.js
@@ -1,8 +1,20 @@
 import React from "react";
 
+import { SearchBar } from "../styles"
+
 const EmployeeSearch = props => {
     return (
-        <h2>EmployeeSearch</h2>
+        // <h2>EmployeeSearch</h2>
+        <SearchBar>
+            <input
+                type="text"
+                name="searchTerm"
+                id="searchTerm"
+                placeholder="Search for employee here"
+                onChange={props.handleInputChange}
+                value={props.searchTerm}
+            />
+        </SearchBar>
     )
 };
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,13 @@ import React from "react";
 
 const NavBar = props => {
     return (
-        <h2>NavBar</h2>
+        // <h2>NavBar</h2>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Employee Directory</h1>
+                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            </div>
+        </div>
     )
 };
 
