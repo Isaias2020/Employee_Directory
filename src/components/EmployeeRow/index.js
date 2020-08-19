93c69019a6b13c7e97a4776b30748bf1aa7393f2
Isaias Quintanilla <quintanillaisaias@gmail.com>
2020-08-19 16:00:43 -0500

    added text for employees, renders all employee photos with info

--- src/components/EmployeeRow/index.js
+++ src/components/EmployeeRow/index.js
@@ -5,6 +5,25 @@ function EmployeeRow(props) {
     const empImage = (id) => {
         switch (id) {
             case 1: return require("./Images/elijah-hiett.jpg");
+            case 2: return require("./Images/alex-bortolato.jpg");
+            case 3: return require("./Images/alexis-chloe.jpg");
+            case 4: return require("./Images/antonio-scant.jpg");
+            case 5: return require("./Images/caleb-lucas.jpg");
+            case 6: return require("./Images/cristian-newman.jpg");
+            case 7: return require("./Images/diya-dunne.jpg");
+            case 8: return require("./Images/emerson-mathew.jpg");
+            case 9: return require("./Images/james-timothy.jpg");
+            case 10: return require("./Images/jesse-ramirez.jpg");
+            case 11: return require("./Images/john-doe.jpg");
+            case 12: return require("./Images/jon-grogan.jpg");
+            case 13: return require("./Images/julian-ferrell.jpg");
+            case 14: return require("./Images/lylah-montoya.jpg");
+            case 15: return require("./Images/mila-rose.jpg");
+            case 16: return require("./Images/milo-farley.jpg");
+            case 17: return require("./Images/monica-dahiya.jpg");
+            case 18: return require("./Images/prijun-koirala.jpg");
+            case 19: return require("./Images/reid-valenzuela.jpg");
+            case 20: return require("./Images/simon-marsault.jpg");
             default: return "https://via.placeholder.com/150";
         }
     }
@@ -19,7 +38,6 @@ function EmployeeRow(props) {
             <td>{props.email}</td>
             <td>{props.dob}</td>
         </tr>
-        // <h2>EmployeeRow</h2>
     );
 
 };
--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -7,7 +7,7 @@ const Heading = props => {
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
                 <h1 className="display-4">Employee Directory</h1>
-                <p className="lead">Click on carrots to filter by heading or use the search box to narrow your results.</p>
+                <p className="lead">Click on buttons to filter by heading or use the search box to narrow your results.</p>
             </div>
             <div className="container">
                 <EmployeeInfo />
