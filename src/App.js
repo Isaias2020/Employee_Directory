9643be691fe89c74d52c49cf2b892a07b228f5d7
Isaias Quintanilla <quintanillaisaias@gmail.com>
2020-08-22 14:58:54 -0500

    fixed empty space issue beneath search results after searching employee

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Heading from "./components/Heading";
+import "./App.css";
 // import EmployeeSearch from "./components/EmployeeSearch";
 // import EmployeeRow from "./components/EmployeeRow";
 
--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./style.css";
 import EmployeeInfo from "../EmployeeInfo";
 
-const Heading = props => {
+const Heading = (props) => {
     return (
-        <div className="jumbotron jumbotron-fluid">
+        <div>
             <div className="container">
                 <h1 className="display-4">Employee Directory</h1>
                 <p className="lead">Click on buttons to filter by heading or use the search box to narrow your results.</p>
